bc0b63610b33ab251d059e0e66bc012956e5f942
Maung Ko Ko <mwkoko2020@gmail.com>
2023-03-21 14:29:18 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+saturdayFun();
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective( symbol ="*") {
+    const innerFunction = function (msg = "Special") {
+      return `You are ${symbol}${msg}${symbol}!`;
+    };
+    return innerFunction;
+  }
+
+wrapAdjective("%")("a dedicated programmer");
